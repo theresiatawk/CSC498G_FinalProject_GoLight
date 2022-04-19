89bf6012281b8231791ea51c433c0daed55bbf52
Theresia Tawk <theresiatawk@hotmail.com>
2022-04-19 08:36:25 +0300

    Splash Screen Implemented

---
+++ Frontend/CSC489G_FinalProject/app/src/main/java/com/lau/csc489g_finalproject/MainActivity.java
@@ -0,0 +1,35 @@
+package com.lau.csc489g_finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    Timer timer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Hiding the Action Bar from the layout
+        getSupportActionBar().hide();
+
+        // After 2 seconds it will move automatically from this activity to the home activity (Splash Screen)
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        }, 2000);
+
+
+    }
+}
