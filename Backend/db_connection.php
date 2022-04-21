e7657b2f3d192219bbcc590be3cb41e57564e0c8
Theresia Tawk <theresiatawk@hotmail.com>
2022-04-21 12:21:34 +0300

    Adding Connection to the database

---
+++ Backend/db_connection.php
@@ -0,0 +1,15 @@
+<?php
+
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = null;
+$db_name = "golightdb";
+
+// Establishing connection between the database and the apis
+$mysqli = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+if(mysqli_connect_errno()){
+    die("Connection_failed!");
+}
+
+?>
