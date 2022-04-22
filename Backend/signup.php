6c503e455e109b6c11598d17cabb8a5bafe5197a
Theresia Tawk <theresiatawk@hotmail.com>
2022-04-22 08:39:03 +0300

    Adjusting the Signup api and fixing errors

--- Backend/signup.php
+++ Backend/signup.php
@@ -10,19 +10,20 @@ function validate($data){
 if (isset($_POST['first_name']) && isset($_POST['last_name']) && isset($_POST['email']) && isset($_POST['password']) && isset($_POST['gender']) && isset($_POST['date_of_birth']) && isset($_POST['height']) && isset($_POST['weight'])){
 
     $first_name = validate($_POST['first_name']);
-    $last_name = vaidate($_POST['last_name']);
-    $email = vaidate($_POST['email']);
+    $last_name = validate($_POST['last_name']);
+    $email = validate($_POST['email']);
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-    $gender = vaidate($_POST['gender']);
-    $date_of_birth = vaidate($_POST['date_of_birth']);
-    $height = vaidate($_POST['height']);
-    $weight = vaidate($_POST['weight']);
+    $gender = validate($_POST['gender']);
+    $date_of_birth = validate($_POST['date_of_birth']);
+    $height = validate($_POST['height']);
+    $weight = validate($_POST['weight']);
 
     if (empty($first_name) || empty($last_name) || empty($email) || empty($password) || empty($gender) || empty($date_of_birth) || empty($height) || empty($weight)) {
         echo("Some field are required");
         exit();
     }
     else{
+    
         $sql1 = "SELECT * FROM users WHERE email ='$email'";
         $result1 = mysqli_query($mysqli, $sql1);
 
@@ -31,13 +32,28 @@ if (isset($_POST['first_name']) && isset($_POST['last_name']) && isset($_POST['e
             exit();
         }
         else{
-            $query = $mysqli->prepare("INSERT INTO users(first_name, last_name, email, password, gender, date_of_birth) VALUES (?, ?, ?);");
-            $query->bind_param("ssssss", $name, $crn, $semster);
+            $query = $mysqli->prepare("INSERT INTO users(first_name, last_name, email, password, gender, date_of_birth) VALUES (?, ?, ?, ?, ?, ?);");
+            $query->bind_param("ssssss", $first_name, $last_name, $email, $password, $gender, $date_of_birth);
             $query->execute();
-            if( mysqli_query($mysqli, $query)){
-                echo "Mabrouk!"
-            }
-            echo "Baddd LucKK!"
+
+            $sql2 = "SELECT user_id FROM users WHERE email ='$email'";
+            $result2 = mysqli_query($mysqli, $sql2);
+            $row = mysqli_fetch_assoc($result2);
+            $id = $row['user_id'];
+
+            date_default_timezone_set('Asia/Beirut');
+
+            $year = intval(date('Y'));
+            $month = intval(date('m'));
+            $day = intval(date('d'));
+
+            $date = strval($day.$month.$year);
+
+            $query1 = $mysqli->prepare("INSERT INTO body(user_id, weight, height, date) VALUES (?, ?, ?, ?);");
+            $query1->bind_param("iiis", $id, $weight, $height, $date);
+            $query1->execute();
+            echo "Mabrouk!";
+
         }
     }
 
