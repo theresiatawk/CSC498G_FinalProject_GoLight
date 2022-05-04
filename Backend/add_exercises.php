4d26a4dc341e93986dec99c87029d8f7cc3b84d1
Theresia Tawk <theresiatawk@hotmail.com>
2022-05-04 10:37:28 +0300

    Updating add_exercise api

--- Backend/add_exercises.php
+++ Backend/add_exercises.php
@@ -7,22 +7,9 @@ function validate($data){
     $data = stripslashes($data);
     return $data;
 }
-if (isset($_POST['user_id']) && isset($_POST['date']) && isset($_POST['running']) && isset($_POST['dancing']) && isset($_POST['boxing']) && isset($_POST['baseball']) && isset($_POST['basketball'])&& isset($_POST['football'])&& isset($_POST['swimming'])&& isset($_POST['skiing'])&& isset($_POST['hiking'])&& isset($_POST['gymnastics'])&& isset($_POST['tennis'])&& isset($_POST['golf'])) {
+if (isset($_POST['user_id']) && isset($_POST['date'])){
     $user = validate($_POST['user_id']);
     $date = validate($_POST['date']);
-    $running = validate($_POST['running']);
-    $dancing = validate($_POST['dancing']);
-    $boxing = validate($_POST['boxing']);
-    $baseball = validate($_POST['baseball']);
-    $basketball = validate($_POST['basketball']);
-    $football = validate($_POST['football']);
-    $swimming = validate($_POST['swimming']);
-    $skiing = validate($_POST['skiing']);
-    $hiking = validate($_POST['hiking']);
-    $gymnastics = validate($_POST['gymnastics']);
-    $tennis = validate($_POST['tennis']);
-    $golf = validate($_POST['golf']);
-
     if (empty($user)) {
         echo "Empty user id";
         exit();
@@ -31,21 +18,231 @@ if (isset($_POST['user_id']) && isset($_POST['date']) && isset($_POST['running']
         echo "Empty date";
         exit();
     }
-    else{
+    else if(isset($_POST['running'])){
+        $running = validate($_POST['running']);
         $sql = "SELECT * FROM exercises WHERE user_id = '" . $user . "' AND date = " . $date . "";
         $result = mysqli_query($mysqli, $sql);
 
         if (mysqli_num_rows($result) != 0) {
-            $query = $mysqli->prepare("UPDATE exercises SET running = ?, dancing = ?, boxing = ?, baseball = ?, basketball = ?, football = ?, swimming = ?, skiing = ?, hiking = ?, gymnastics = ?, tennis = ?, golf = ? WHERE user_id = ? AND date = ?;");
-            $query->bind_param("ssssssssssssis", $running, $dancing, $boxing, $baseball, $basketball, $football, $swimming, $skiing, $hiking, $gymnastics, $tennis, $golf, $user, $date);
+            $query = $mysqli->prepare("UPDATE exercises SET running = ? Where user_id = ? AND date = ?;");
+            $query->bind_param("sis", $running, $user, $date);
             $query->execute();
             echo "Updated!";
         }
         else{
-            $query = $mysqli->prepare("INSERT INTO exercises(user_id, running, dancing, boxing, baseball, basketball, football, swimming, skiing, hiking, gymnastics, tennis, golf, date) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);");
-            $query->bind_param("isssssssssssss", $user, $running, $dancing, $boxing, $baseball, $basketball, $football, $swimming, $skiing, $hiking, $gymnastics, $tennis, $golf, $date);
+            $query = $mysqli->prepare("INSERT INTO exercises(user_id, running, date) VALUES (?, ?, ?);");
+            $query->bind_param("iss", $user, $running, $date);
             $query->execute();
             echo "Data Added!";
+
+        }
+    } 
+    else if (isset($_POST['dancing'])){
+        $dancing = validate($_POST['dancing']);
+        $sql = "SELECT * FROM exercises WHERE user_id = '" . $user . "' AND date = " . $date . "";
+        $result = mysqli_query($mysqli, $sql);
+
+        if (mysqli_num_rows($result) != 0) {
+            $query = $mysqli->prepare("UPDATE exercises SET dancing = ? WHERE user_id = ? AND date = ?;");
+            $query->bind_param("sis", $dancing, $user, $date);
+            $query->execute();
+            echo "Updated!";
+        }
+        else{
+            $query = $mysqli->prepare("INSERT INTO exercises(user_id, dancing, date) VALUES (?, ?, ?);");
+            $query->bind_param("iss", $user, $dancing, $date);
+            $query->execute();
+            echo "Data Added!";
+
+        }
+    }
+    else if (isset($_POST['boxing'])){
+        $boxing = validate($_POST['boxing']);
+        $sql = "SELECT * FROM exercises WHERE user_id = '" . $user . "' AND date = " . $date . "";
+        $result = mysqli_query($mysqli, $sql);
+
+        if (mysqli_num_rows($result) != 0) {
+            $query = $mysqli->prepare("UPDATE exercises SET boxing = ? WHERE user_id = ? AND date = ?;");
+            $query->bind_param("sis", $boxing, $user, $date);
+            $query->execute();
+            echo "Updated!";
+        }
+        else{
+            $query = $mysqli->prepare("INSERT INTO exercises(user_id, boxing, date) VALUES (?, ?, ?);");
+            $query->bind_param("iss", $user, $boxing, $date);
+            $query->execute();
+            echo "Data Added!";
+
+        }
+    }
+    else if (isset($_POST['baseball'])){
+        $baseball = validate($_POST['baseball']);
+        $sql = "SELECT * FROM exercises WHERE user_id = '" . $user . "' AND date = " . $date . "";
+        $result = mysqli_query($mysqli, $sql);
+
+        if (mysqli_num_rows($result) != 0) {
+            $query = $mysqli->prepare("UPDATE exercises SET baseball = ? WHERE user_id = ? AND date = ?;");
+            $query->bind_param("sis", $baseball, $user, $date);
+            $query->execute();
+            echo "Updated!";
+        }
+        else{
+            $query = $mysqli->prepare("INSERT INTO exercises(user_id, baseball, date) VALUES (?, ?, ?);");
+            $query->bind_param("iss", $user, $baseball, $date);
+            $query->execute();
+            echo "Data Added!";
+
+        }
+    } 
+    else if (isset($_POST['basketball'])){
+        $basketball = validate($_POST['basketball']);
+        $sql = "SELECT * FROM exercises WHERE user_id = '" . $user . "' AND date = " . $date . "";
+        $result = mysqli_query($mysqli, $sql);
+
+        if (mysqli_num_rows($result) != 0) {
+            $query = $mysqli->prepare("UPDATE exercises SET basketball = ? WHERE user_id = ? AND date = ?;");
+            $query->bind_param("sis", $basketball, $user, $date);
+            $query->execute();
+            echo "Updated!";
+        }
+        else{
+            $query = $mysqli->prepare("INSERT INTO exercises(user_id, basketball, date) VALUES (?, ?, ?);");
+            $query->bind_param("iss", $user, $basketball, $date);
+            $query->execute();
+            echo "Data Added!";
+
+        }
+    }
+    else if (isset($_POST['football'])){
+        $football = validate($_POST['football']);
+        $sql = "SELECT * FROM exercises WHERE user_id = '" . $user . "' AND date = " . $date . "";
+        $result = mysqli_query($mysqli, $sql);
+
+        if (mysqli_num_rows($result) != 0) {
+            $query = $mysqli->prepare("UPDATE exercises SET football = ? WHERE user_id = ? AND date = ?;");
+            $query->bind_param("sis", $football, $user, $date);
+            $query->execute();
+            echo "Updated!";
+        }
+        else{
+            $query = $mysqli->prepare("INSERT INTO exercises(user_id, football, date) VALUES (?, ?, ?);");
+            $query->bind_param("iss", $user, $football, $date);
+            $query->execute();
+            echo "Data Added!";
+
+        }
+    }
+    else if (isset($_POST['swimming'])){
+        $swimming = validate($_POST['swimming']);
+        $sql = "SELECT * FROM exercises WHERE user_id = '" . $user . "' AND date = " . $date . "";
+        $result = mysqli_query($mysqli, $sql);
+
+        if (mysqli_num_rows($result) != 0) {
+            $query = $mysqli->prepare("UPDATE exercises SET swimming = ? WHERE user_id = ? AND date = ?;");
+            $query->bind_param("sis", $swimming, $user, $date);
+            $query->execute();
+            echo "Updated!";
+        }
+        else{
+            $query = $mysqli->prepare("INSERT INTO exercises(user_id, swimming, date) VALUES (?, ?, ?);");
+            $query->bind_param("iss", $user, $swimming, $date);
+            $query->execute();
+            echo "Data Added!";
+
+        }
+    }
+    else if (isset($_POST['skiing'])){
+        $skiing = validate($_POST['skiing']);
+        $sql = "SELECT * FROM exercises WHERE user_id = '" . $user . "' AND date = " . $date . "";
+        $result = mysqli_query($mysqli, $sql);
+
+        if (mysqli_num_rows($result) != 0) {
+            $query = $mysqli->prepare("UPDATE exercises SET skiing = ? WHERE user_id = ? AND date = ?;");
+            $query->bind_param("sis", $skiing, $user, $date);
+            $query->execute();
+            echo "Updated!";
+        }
+        else{
+            $query = $mysqli->prepare("INSERT INTO exercises(user_id, skiing, date) VALUES (?, ?, ?);");
+            $query->bind_param("iss", $user, $skiing, $date);
+            $query->execute();
+            echo "Data Added!";
+
+        }
+    }
+    else if (isset($_POST['hiking'])){
+        $hiking = validate($_POST['hiking']);
+        $sql = "SELECT * FROM exercises WHERE user_id = '" . $user . "' AND date = " . $date . "";
+        $result = mysqli_query($mysqli, $sql);
+
+        if (mysqli_num_rows($result) != 0) {
+            $query = $mysqli->prepare("UPDATE exercises SET hiking = ? WHERE user_id = ? AND date = ?;");
+            $query->bind_param("sis", $hiking, $user, $date);
+            $query->execute();
+            echo "Updated!";
+        }
+        else{
+            $query = $mysqli->prepare("INSERT INTO exercises(user_id, hiking, date) VALUES (?, ?, ?);");
+            $query->bind_param("iss", $user, $hiking, $date);
+            $query->execute();
+            echo "Data Added!";
+
+        }
+    }
+    else if (isset($_POST['gymnastics'])){
+        $gymnastics = validate($_POST['gymnastics']);
+        $sql = "SELECT * FROM exercises WHERE user_id = '" . $user . "' AND date = " . $date . "";
+        $result = mysqli_query($mysqli, $sql);
+
+        if (mysqli_num_rows($result) != 0) {
+            $query = $mysqli->prepare("UPDATE exercises SET gymnastics = ? WHERE user_id = ? AND date = ?;");
+            $query->bind_param("sis", $gymnastics, $user, $date);
+            $query->execute();
+            echo "Updated!";
+        }
+        else{
+            $query = $mysqli->prepare("INSERT INTO exercises(user_id, gymnastics, date) VALUES (?, ?, ?);");
+            $query->bind_param("iss", $user, $gymnastics, $date);
+            $query->execute();
+            echo "Data Added!";
+
+        }
+    }
+    else if (isset($_POST['tennis'])){
+        $tennis = validate($_POST['tennis']);
+        $sql = "SELECT * FROM exercises WHERE user_id = '" . $user . "' AND date = " . $date . "";
+        $result = mysqli_query($mysqli, $sql);
+
+        if (mysqli_num_rows($result) != 0) {
+            $query = $mysqli->prepare("UPDATE exercises SET tennis = ? WHERE user_id = ? AND date = ?;");
+            $query->bind_param("sis", $tennis, $user, $date);
+            $query->execute();
+            echo "Updated!";
+        }
+        else{
+            $query = $mysqli->prepare("INSERT INTO exercises(user_id, tennis, date) VALUES (?, ?, ?);");
+            $query->bind_param("iss", $user, $tennis, $date);
+            $query->execute();
+            echo "Data Added!";
+        }
+    }
+    else if (isset($_POST['golf'])) {
+        $golf = validate($_POST['golf']);
+        $sql = "SELECT * FROM exercises WHERE user_id = '" . $user . "' AND date = " . $date . "";
+        $result = mysqli_query($mysqli, $sql);
+
+        if (mysqli_num_rows($result) != 0) {
+            $query = $mysqli->prepare("UPDATE exercises SET golf = ? WHERE user_id = ? AND date = ?;");
+            $query->bind_param("sis", $golf, $user, $date);
+            $query->execute();
+            echo "Updated!";
+        }
+        else{
+            $query = $mysqli->prepare("INSERT INTO exercises(user_id, golf, date) VALUES (?, ?, ?);");
+            $query->bind_param("iss", $user, $golf, $date);
+            $query->execute();
+            echo "Data Added!";
+
         }
     }
 }
--- Frontend/CSC489G_FinalProject/app/src/main/java/com/lau/csc489g_finalproject/WhatExerciseActivity.java
+++ Frontend/CSC489G_FinalProject/app/src/main/java/com/lau/csc489g_finalproject/WhatExerciseActivity.java
@@ -3,15 +3,195 @@ package com.lau.csc489g_finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
 
 public class WhatExerciseActivity extends AppCompatActivity {
 
     TextView exercise_header;
     EditText exercise_info;
+    String header_to_display;
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+        protected String doInBackground(String... params) {
+            String first_param = params[0];
+            String second_param = params[1];
+            String third_param = params[2];
+
+            URL url;
+            HttpURLConnection http;
+
+            try{
+                url = new URL(params[3]);
+
+                // Opening a connection between android app and the url
+                http = (HttpURLConnection) url.openConnection();
+
+                http.setRequestMethod("POST");
+                http.setDoInput(true);
+                http.setDoOutput(true);
+
+                // I need an Output Stream to sent params to the API
+                OutputStream out_stream = http.getOutputStream();
+                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(out_stream, "UTF-8"));
+
+                String post1 = URLEncoder.encode("user_id", "UTF-8")+"="+ URLEncoder.encode(first_param, "UTF-8")+"&"+URLEncoder.encode("date", "UTF-8")+"="+ URLEncoder.encode(second_param, "UTF-8")+"&"+URLEncoder.encode("running", "UTF-8")+"="+ URLEncoder.encode(third_param, "UTF-8");
+                String post2 = URLEncoder.encode("user_id", "UTF-8")+"="+ URLEncoder.encode(first_param, "UTF-8")+"&"+URLEncoder.encode("date", "UTF-8")+"="+ URLEncoder.encode(second_param, "UTF-8")+"&"+URLEncoder.encode("dancing", "UTF-8")+"="+ URLEncoder.encode(third_param, "UTF-8");
+                String post3 = URLEncoder.encode("user_id", "UTF-8")+"="+ URLEncoder.encode(first_param, "UTF-8")+"&"+URLEncoder.encode("date", "UTF-8")+"="+ URLEncoder.encode(second_param, "UTF-8")+"&"+URLEncoder.encode("boxing", "UTF-8")+"="+ URLEncoder.encode(third_param, "UTF-8");
+                String post4 = URLEncoder.encode("user_id", "UTF-8")+"="+ URLEncoder.encode(first_param, "UTF-8")+"&"+URLEncoder.encode("date", "UTF-8")+"="+ URLEncoder.encode(second_param, "UTF-8")+"&"+URLEncoder.encode("baseball", "UTF-8")+"="+ URLEncoder.encode(third_param, "UTF-8");
+                String post5 = URLEncoder.encode("user_id", "UTF-8")+"="+ URLEncoder.encode(first_param, "UTF-8")+"&"+URLEncoder.encode("date", "UTF-8")+"="+ URLEncoder.encode(second_param, "UTF-8")+"&"+URLEncoder.encode("basketball", "UTF-8")+"="+ URLEncoder.encode(third_param, "UTF-8");
+                String post6 = URLEncoder.encode("user_id", "UTF-8")+"="+ URLEncoder.encode(first_param, "UTF-8")+"&"+URLEncoder.encode("date", "UTF-8")+"="+ URLEncoder.encode(second_param, "UTF-8")+"&"+URLEncoder.encode("football", "UTF-8")+"="+ URLEncoder.encode(third_param, "UTF-8");
+                String post7 = URLEncoder.encode("user_id", "UTF-8")+"="+ URLEncoder.encode(first_param, "UTF-8")+"&"+URLEncoder.encode("date", "UTF-8")+"="+ URLEncoder.encode(second_param, "UTF-8")+"&"+URLEncoder.encode("swimming", "UTF-8")+"="+ URLEncoder.encode(third_param, "UTF-8");
+                String post8 = URLEncoder.encode("user_id", "UTF-8")+"="+ URLEncoder.encode(first_param, "UTF-8")+"&"+URLEncoder.encode("date", "UTF-8")+"="+ URLEncoder.encode(second_param, "UTF-8")+"&"+URLEncoder.encode("skiing", "UTF-8")+"="+ URLEncoder.encode(third_param, "UTF-8");
+                String post9 = URLEncoder.encode("user_id", "UTF-8")+"="+ URLEncoder.encode(first_param, "UTF-8")+"&"+URLEncoder.encode("date", "UTF-8")+"="+ URLEncoder.encode(second_param, "UTF-8")+"&"+URLEncoder.encode("hiking", "UTF-8")+"="+ URLEncoder.encode(third_param, "UTF-8");
+                String post10 = URLEncoder.encode("user_id", "UTF-8")+"="+ URLEncoder.encode(first_param, "UTF-8")+"&"+URLEncoder.encode("date", "UTF-8")+"="+ URLEncoder.encode(second_param, "UTF-8")+"&"+URLEncoder.encode("gymnastics", "UTF-8")+"="+ URLEncoder.encode(third_param, "UTF-8");
+                String post11 = URLEncoder.encode("user_id", "UTF-8")+"="+ URLEncoder.encode(first_param, "UTF-8")+"&"+URLEncoder.encode("date", "UTF-8")+"="+ URLEncoder.encode(second_param, "UTF-8")+"&"+URLEncoder.encode("tennis", "UTF-8")+"="+ URLEncoder.encode(third_param, "UTF-8");
+                String post12 = URLEncoder.encode("user_id", "UTF-8")+"="+ URLEncoder.encode(first_param, "UTF-8")+"&"+URLEncoder.encode("date", "UTF-8")+"="+ URLEncoder.encode(second_param, "UTF-8")+"&"+URLEncoder.encode("golf", "UTF-8")+"="+ URLEncoder.encode(third_param, "UTF-8");
+
+                if (header_to_display.equalsIgnoreCase("running")){
+                    bw.write(post1);
+                }
+                else if (header_to_display.equalsIgnoreCase("dancing")){
+                    bw.write(post2);
+                }
+                else if (header_to_display.equalsIgnoreCase("boxing")){
+                    bw.write(post3);
+                }
+                else if (header_to_display.equalsIgnoreCase("boxing")){
+                    bw.write(post4);
+                }
+                else if (header_to_display.equalsIgnoreCase("boxing")){
+                    bw.write(post5);
+                }
+                else if (header_to_display.equalsIgnoreCase("boxing")){
+                    bw.write(post6);
+                }
+                else if (header_to_display.equalsIgnoreCase("boxing")){
+                    bw.write(post7);
+                }
+                else if (header_to_display.equalsIgnoreCase("boxing")){
+                    bw.write(post8);
+                }
+                else if (header_to_display.equalsIgnoreCase("boxing")){
+                    bw.write(post9);
+                }
+                else if (header_to_display.equalsIgnoreCase("boxing")){
+                    bw.write(post10);
+                }
+                else if (header_to_display.equalsIgnoreCase("boxing")){
+                    bw.write(post3);
+                }
+                else if (header_to_display.equalsIgnoreCase("boxing")){
+                    bw.write(post3);
+                }
+                else if (header_to_display.equalsIgnoreCase("boxing")){
+                    bw.write(post3);
+                }
+                bw.flush();
+                bw.close();
+                out_stream.close();
+
+                // Reading the result from the API
+                InputStream in_stream = http.getInputStream();
+                BufferedReader br = new BufferedReader(new InputStreamReader(in_stream, "iso-8859-1"));
+                String result = "";
+                String line = "";
+                while((line = br.readLine())!= null){
+                    result += line;
+                }
+                br.close();
+                in_stream.close();
+                http.disconnect();
+                return result;
+            }
+            catch(Exception e){
+                e.printStackTrace();
+                return null;
+            }
+        }
+        protected void onPostExecute(String result){
+            Intent intent = new Intent(getApplicationContext(), WhatExerciseActivity.class);
+            super.onPostExecute(result);
+            //If result incorrect print a toast
+            if(result.equals("No data")){
+                Toast.makeText(getApplicationContext(),"No data on this day", Toast.LENGTH_LONG).show();
+            }
+            // If result correct convert the received json object to string
+            else{
+                try{
+                    JSONArray array = new JSONArray(result);
+                    ArrayList<Object> list = new ArrayList<>();
+                    JSONObject obj;
+
+                    for (int i = 0; i < array.length(); i ++){
+                        list.add(array.get(i));
+                    }
+                    running = new String[array.length()];
+                    dancing = new String[array.length()];
+                    boxing = new String[array.length()];
+                    baseball = new String[array.length()];
+                    basketball = new String[array.length()];
+                    football = new String[array.length()];
+                    swimming = new String[array.length()];
+                    skiing = new String[array.length()];
+                    hiking = new String[array.length()];
+                    gymnastics = new String[array.length()];
+                    tennis = new String[array.length()];
+                    golf = new String[array.length()];
+
+                    obj = (JSONObject) array.get(0);
+                    running[0] = obj.getString("running");
+                    dancing[0] = obj.getString("dancing");
+                    boxing[0] = obj.getString("boxing");
+                    baseball[0] = obj.getString("baseball");
+                    basketball[0] = obj.getString("basketball");
+                    football[0] = obj.getString("football");
+                    swimming[0] = obj.getString("swimming");
+                    skiing[0] = obj.getString("skiing");
+                    hiking[0] = obj.getString("hiking");
+                    gymnastics[0] = obj.getString("gymnastics");
+                    tennis[0] = obj.getString("tennis");
+                    golf[0] = obj.getString("golf");
+
+                    intent.putExtra("running", running[0]);
+                    intent.putExtra("dancing", dancing[0]);
+                    intent.putExtra("boxing", boxing[0]);
+                    intent.putExtra("baseball", baseball[0]);
+                    intent.putExtra("basketball", basketball[0]);
+                    intent.putExtra("football", football[0]);
+                    intent.putExtra("swimming", swimming[0]);
+                    intent.putExtra("skiing", skiing[0]);
+                    intent.putExtra("hiking", hiking[0]);
+                    intent.putExtra("gymnastics", gymnastics[0]);
+                    intent.putExtra("tennis", tennis[0]);
+                    intent.putExtra("golf", golf[0]);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            intent.putExtra("destination", destination);
+            startActivity(intent);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +205,7 @@ public class WhatExerciseActivity extends AppCompatActivity {
         exercise_info = (EditText) findViewById(R.id.exercise_info);
         // Getting the tag transferred from Food activity page
         Intent intent = getIntent();
-        String header_to_display = intent.getStringExtra("destination");
+        header_to_display = intent.getStringExtra("destination");
 
         String running = intent.getStringExtra("running");
         String dancing = intent.getStringExtra("dancing");
